8ca10b0638d91cab02cb5f9ef27d5254896692f6
Monarca117 <skyggemonarken@gmail.com>
2024-04-12 08:42:24 -0600

    versión 1.0.0.0

---
+++ SXMLJugador/CJugador.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization.Formatters.Binary;  //El formateador binario toma la información de la memoria bit pot bi y lo guarda.
+using System.IO;
+using System.Reflection;
+
+namespace SXMLJugador
+{
+    [Serializable]
+    public class CJugador
+    {
+        private String name;
+        private String role;
+        private int level;
+
+        public String Name { set { name = value; } get { return name; } }
+        public String Role { set { role = value; } get { return role; } }
+        public int Level { set { level = value; } get { return level; } }
+
+        //Constructor vacio
+        public CJugador() 
+        {
+            name = "Monarca";
+            role = "Asesino";
+            level = 15;
+        }
+
+        public void MuestraInformacion()
+        {
+            //Mostramos la informacion necesaria
+            Console.WriteLine("Nombre de jugador: {0}", name);
+            Console.WriteLine("Rol del jugador: {0}", role);
+            Console.WriteLine("Nivel: {0}", level);
+            Console.WriteLine("------------------");
+        }
+    }
+}
---
+++ SXMLJugador/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SXMLJugador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            String valor = "";
+
+            Console.WriteLine("1) Crear y serializar jugador, 2) Leer jugador");
+            valor = Console.ReadLine();
+            opcion = Convert.ToInt32(valor);
+
+            if (opcion == 1)
+            {
+                //Creamos el objeto CJugador
+                string name = "";
+                string role = "";
+                int level = 0;
+
+                Console.WriteLine("Escribe el nombre del jugador:");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Escribe el rol del jugador: ");
+                role = Console.ReadLine();
+
+                Console.WriteLine("Escribe el nivel del jugador:");
+                valor = Console.ReadLine();
+                level = Convert.ToInt32(valor);
+
+                CJugador miJugador = new CJugador();
+                miJugador.Name = name;
+                miJugador.Role = role;
+                miJugador.Level = level;
+
+                Console.WriteLine("Jugador a serializar");
+                miJugador.MuestraInformacion();
+
+                //Empezamos la serializacion
+                Console.WriteLine("---SERIALIZAMOS---");
+
+                //Sellecionamos el formateador
+                XmlSerializer formateador = new XmlSerializer(typeof(CJugador));
+
+                //Se crea el stream
+                Stream miStream = new FileStream("Jugador.jug", FileMode.Create, FileAccess.Write, FileShare.None);
+
+                //Serializamos
+                formateador.Serialize(miStream, miJugador);
+
+                //Cerramos el stream
+                miStream.Close();
+            }
+
+            if (opcion == 2)
+            {
+                //Deserealizamos el objeto
+                Console.WriteLine("---DESEREALIZANDO---");
+
+                //Seleccionamos el formateador
+                XmlSerializer formateador = new XmlSerializer(typeof(CJugador));
+
+                //Creamos el Stream
+                Stream miStream = new FileStream("Jugador.jug", FileMode.Open, FileAccess.Read, FileShare.None);
+
+                //Deserializamos
+                CJugador miJugador = (CJugador)formateador.Deserialize(miStream);
+
+                //Cerramos el stream
+                miStream.Close();
+
+                //Usamos el objeto
+                Console.WriteLine("El auto deserializado es: ");
+                miJugador.MuestraInformacion();
+            }
+        }
+    }
+}
